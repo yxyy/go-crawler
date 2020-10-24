75c7fd51cf09794a75405d6c3226cc8a252128f9
一夕烟云 <1043885672@qq.com>
2020-10-24 11:13:27 +0800

    入口

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"lhc.go.crawler/confs"
 	"lhc.go.crawler/libs/mysql"
 	"lhc.go.crawler/router"
-	"log"
+	"lhc.go.crawler/logs"
 )
 
 func main()  {
 
+	fmt.Println("加载日志组件.....")
+	if err:=logs.InitLogs();err!=nil {
+		logs.Fatal.Fatal(err)
+	}
+	logs.Error.Info("log init")
+
 	//加载配置
+	fmt.Println("加载配置组件.....")
 	if err := confs.InitConfs();err!=nil{
-		log.Fatal(err)
+		logs.Fatal.Fatal(err)
 	}
 	//加载mysql连接
+	fmt.Println("加载mysql连接组件.....")
 	if err := mysql.InitMysql();err != nil {
-		log.Fatal(err)
+		logs.Fatal.Fatal(err)
 	}
 	defer mysql.MysqlConnet.Close()
 
 	//加载路由
+	fmt.Println("加载路由组件.....")
 	r := router.InitRouter()
 	if err := r.Run(":8080");err!=nil{
-		log.Fatal(err)
+		logs.Fatal.Fatal(err)
 	}
 }
