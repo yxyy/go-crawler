be368ead1dd8e0072a465b699ea0acff8880062a
一夕烟云 <1043885672@qq.com>
2020-10-27 15:40:19 +0800

    main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"lhc.go.crawler/confs"
+	"lhc.go.crawler/controller/corn"
 	"lhc.go.crawler/libs/mysql"
 	"lhc.go.crawler/libs/es"
 	"lhc.go.crawler/router"
@@ -14,6 +15,7 @@ func main()  {
 	fmt.Println("加载日志组件.....")
 	if err:=logs.InitLogs();err!=nil {
 		logs.Fatal.Fatal(err)
+		fmt.Println(err)
 	}
 	logs.Error.Info("log init")
 
@@ -21,11 +23,13 @@ func main()  {
 	fmt.Println("加载配置组件.....")
 	if err := confs.InitConfs();err!=nil{
 		logs.Fatal.Fatal(err)
+		fmt.Println(err)
 	}
 	//加载mysql连接
 	fmt.Println("加载mysql连接组件.....")
 	if err := mysql.InitMysql();err != nil {
 		logs.Fatal.Fatal(err)
+		fmt.Println(err)
 	}
 	defer mysql.MysqlConnet.Close()
 
@@ -33,12 +37,17 @@ func main()  {
 	fmt.Println("加载es连接组件.....")
 	if err := es.InitEs();err != nil {
 		logs.Fatal.Fatal(err)
+		fmt.Println(err)
 	}
+
+	//启动定时任务
+	go corn.InitCornRouter()
 
 	//加载路由
 	fmt.Println("加载路由组件.....")
 	r := router.InitRouter()
 	if err := r.Run(":8080");err!=nil{
 		logs.Fatal.Fatal(err)
+		fmt.Println(err)
 	}
 }
